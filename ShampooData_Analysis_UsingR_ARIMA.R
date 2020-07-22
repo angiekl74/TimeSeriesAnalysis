70f2e07a27854f65b44091ef187fb2cd9daab898
angiekl74 <angiekl74@hotmail.com>
2020-07-21 20:35:07 -0700

    Initial commit Portland Ridership Analysis SARIMAX model

---
+++ ShampooData_Analysis_UsingR_ARIMA.R
@@ -0,0 +1,32 @@
+# Set directory and read in csv file
+setwd("C:/Users/Lenovo/GitHub/TimeSeriesAnalysis")
+shampoo <- read.csv("Resources/ShampooData.csv", stringsAsFactors = FALSE)
+
+# Rename columns
+names(shampoo)<-c("month","sales")
+
+# Check datatypes
+str(shampoo)
+
+# ggplot library
+library(ggplot2)
+
+# General plot for trend?
+ggplot(data=shampoo, aes(x=month, y=sales, group=1)) +
+  +     geom_line()+
+  +     geom_point()
+
+# Change to ts() format
+sales<-ts(shampoo$sales)
+
+# Install forecast package
+install.packages("forecast")
+
+# Find optimal model
+salesOPT<-auto.arima(sales)
+
+# Forecast out 12 months
+SalesForecast<-forecast(object = salesOPT, h=12)
+
+# Plot 12 month forecast
+plot(SalesForecast)
